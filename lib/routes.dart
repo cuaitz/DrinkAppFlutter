fc6d54a8d5599d6d0601bfe19c223c06c50423ef
cuaitz <ocuait@gmail.com>
2026-04-29 20:14:54 -0300

    [FEAT] Base do GoRouter

---
+++ lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:go_router/go_router.dart';
+
+class DrinkAppRoutes {
+
+}
+
+final GoRouter drinkAppRouter = GoRouter(
+  initialLocation: null, // to-do: trocar pela home
+  routes: [
+
+  ]
+);
