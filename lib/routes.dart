af8d97ceb1efe7954bd12f01ec64d98662f94e9d
Augusto Fogaça <98088769+cuaitz@users.noreply.github.com>
2026-05-02 17:09:57 -0300

    [FEAT] Tela de login

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,18 +1,27 @@
 import 'package:drink_app_flutter/view/home_view.dart';
+import 'package:drink_app_flutter/view/login_view.dart';
 import 'package:go_router/go_router.dart';
 
 class DrinkAppRoutes {
   static const String homeView = '/home';
+  static const String loginView = '/login';
 }
 
 final GoRouter drinkAppRouter = GoRouter(
-  initialLocation: DrinkAppRoutes.homeView,
+  initialLocation: DrinkAppRoutes.loginView,
   routes: [
     GoRoute(
       path: DrinkAppRoutes.homeView,
       name: DrinkAppRoutes.homeView,
       builder: (context, state) {
         return HomeView();
+      }
+    ),
+    GoRoute(
+      path: DrinkAppRoutes.loginView,
+      name: DrinkAppRoutes.loginView,
+      builder: (context, state) {
+        return LoginView();
       }
     )
   ]
---
+++ lib/view/login_view.dart
@@ -0,0 +1,53 @@
+import 'package:drink_app_flutter/view/components/drink_button.dart';
+import 'package:drink_app_flutter/view/components/drink_text_field.dart';
+import 'package:drink_app_flutter/view/default_view.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _onLogin() {
+    // TO-DO: Implementar a lógica de login
+
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    print("Username: $username, Password: $password");
+
+    GoRouter.of(context).pushNamed('/home');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultView(
+      title: "Login",
+      body: Center(
+        child: Column(
+          children: [
+            Text("Login"),
+            Text("This is the login page"),
+            DrinkTextField(
+              hintText: "Username",
+              controller: _usernameController,
+            ),
+            DrinkTextField(
+              hintText: "Password",
+              controller: _passwordController,
+              obscureText: true,
+            ),
+            DrinkButton(text: 'Login', onPressed: _onLogin)
+          ]
+        )
+      ),
+    );
+  }
+}
